759eddce7d78eb37844ec14c8dcfdde873a9ed86
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-11-30 22:16:49 -0500

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+    res.send('hello')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server started on port ${process.env.PORT}`)
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Routes, Route } from 'react-router-dom'
+import { Container } from "react-bootstrap"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>APP</h1>
+    </div>
+  )
+}
+
+export default App
