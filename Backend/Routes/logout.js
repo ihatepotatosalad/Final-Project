8de5052731eed06824f0715cbf8ff7f9e34354cf
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-12-05 22:08:01 -0500

    added persist hook

--- Backend/Routes/logout.js
+++ Backend/Routes/logout.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router()
 const logoutController = require('../Controllers/logoutController')
 
-router.get('/', logoutController.handleLogout)
+router.post('/', logoutController.handleLogout)
 module.exports = router
